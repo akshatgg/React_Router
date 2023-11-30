c607d4d2d23d1f01901a6b27ea812126c2d6de58
akshatgg <akshatg9636@gmail.com>
2023-12-01 00:43:37 +0545

    adding every component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+const router=createBrowserRouter()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider/>
   </React.StrictMode>
 );
 
