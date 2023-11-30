bb91b8ed8e93ac4db809b57c4b4875428e0bc4b1
akshatgg <akshatg9636@gmail.com>
2023-12-01 02:55:42 +0545

    using Router

--- src/index.js
+++ src/index.js
@@ -2,27 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Home from './component/Home/Home.js';
 import About from './component/About/About.js';
+import Github from './component/Github/Github';
+import Contact from './component/Contact/Contact';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App/>,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home/>,
+//       },
+//       {
+//         path: 'About',
+//         element: <About />,
+//       },
+//       {                                                                           
+//         path: 'Github',
+//         element:<Github/>
+//       },
+//       {
+//         path: 'Contact',
+//         element:<Contact/>
+//       }
+//     ],
+//   },
+// ]);
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>,
-    children: [
-      {
-        path: '',
-        element: <Home/>,
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-    ],
-  },
-]);
+
+// 2 Method
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    
+    <Route path='/' element={<App/>}>
+    <Route path='' element={<Home/>}/>
+    <Route path='Contact' element={<Contact/>}/>
+
+    <Route path='About' element={<About/>}/>
+    <Route path='Github' element={<Github/>}/>
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
